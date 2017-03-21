471ed2d2524e808997bb1af8fb0f0dc87ae4b27a
A_Elite <AELe@users.noreply.github.com>
2017-03-21 17:00:08 +0800

    Update mapView.h
    添加了一些关于图形缩放和移动的变量

--- mapView.h
+++ mapView.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #define POINTNUM 6
+#include "vector"
 struct MYPOINT
 {
 	double longitude;
@@ -14,6 +15,13 @@ class CmapView : public CView
 {
 private:
 	MYPOINT mypoint[POINTNUM];
+	BOOL	m_bMouseDrag;//判断鼠标是否被拖拽的布尔型变量
+	BOOL	m_bMouseClick;
+	POINT   m_ptDragStart;//鼠标第一次点击地图时的坐标
+	POINT   m_ptDragOffset;//鼠标第二次点击与第一次点击的偏移量
+	BOOL    m_reDrawFlag;
+	POINT * temppt = new POINT[POINTNUM];
+	double   scale;
 protected: // create from serialization only
 	CmapView();
 	DECLARE_DYNCREATE(CmapView)
@@ -47,6 +55,11 @@ protected:
 // Generated message map functions
 protected:
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
 };
 
 #ifndef _DEBUG  // debug version in mapView.cpp
